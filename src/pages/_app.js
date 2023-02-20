f43e1b35b52638b7af4b978016f3c6050ff15560
vishnu <vishnu@delighteck.com>
2023-02-20 14:24:25 +0800

    Configure Amplify on Frontend

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,6 +1,11 @@
 import "@/styles/globals.css";
 import { Header } from "@/components/Header";
 import { Footer } from "@/components/Footer";
+import Amplify, { Hub } from "@aws-amplify/core";
+
+import awsconfig from "../aws-exports";
+
+Amplify.configure(awsconfig);
 
 export default function App({ Component, pageProps }) {
   return (
